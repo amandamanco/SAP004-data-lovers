55de040efac4970c31f2fd6e7a1eebbd79f373fe
Amanda Manço <amand113@hotmail.com>
2020-05-04 00:28:57 -0300

    Visualização dos Pokemons - Numero, Nome e Tipo

--- src/data.js
+++ src/data.js
@@ -1,10 +1,5 @@
-// funções de exemplo
+import pokemon from './data/pokemon/pokemon.js';
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
 
+  
--- src/main.js
+++ src/main.js
@@ -1,6 +1,66 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import pokemon from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+let pokemonData = data.pokemon;
+
+pokemonData.map((pokemon) => {
+    let card = document.createElement('div');
+    let img = document.createElement('img');
+    let box = document.createElement('div');
+    let number = document.createElement('p');
+    let type = document.createElement('p');
+    let name = document.createElement('p');
+
+    img.src = pokemon.img;
+    number.innerHTML = `${pokemon.num}`;
+    type.innerHTML = `${pokemon.type}`;
+
+    // if (pokemon.type[1] !== undefined) {
+    //     let typeTwo = document.createElement('p');
+    //     typeTwo.innerHTML = `${pokemon.type[1]}`;
+    //     card.classList.add("type");
+    //     box.appendChild(typeTwo);
+    // }
+
+    name.innerHTML = `${pokemon.name}`;
+    if (pokemon.id === 29) {
+        name.innerHTML = "Nidoran ♀";
+    } else if (pokemon.id === 32) {
+        name.innerHTML = "Nidoran ♂ "
+    }
+
+    card.classList.add("card");
+    img.classList.add("img");
+    box.classList.add("box")
+    number.classList.add("number");
+    type.classList.add("type");
+    name.classList.add("name");
+
+    // switch (pokemon.type[0]) {
+    //     case 'Grass':
+    //         type.style.backgrounColor = "#10DB0C";
+    //         break;
+    //     case 'Poison':
+    //         type.style.backgroundColor = "#9D00FD";
+    //         break;
+    //     case 'Fire':
+    //         type.style.backgrundColor = "#FF0F0F"
+    //         break;
+    //     default:
+    //         type.style.background = "#ffff"
+    // }
+
+
+    card.appendChild(img);
+    card.appendChild(box);
+    box.appendChild(number);
+    box.appendChild(type);
+    card.appendChild(name);
+
+    document.getElementById("root").appendChild(card);
+})
+
+
+
+
+
