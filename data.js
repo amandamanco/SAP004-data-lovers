b6952d727ed130861e88ad86c89a6ead58ab585b
Amanda Manço <amand113@hotmail.com>
2020-05-19 22:45:32 -0300

    Updates

--- data.js
+++ data.js
@@ -6,21 +6,26 @@ export const filterData = (typeFilter, allPokemons) => {
     return filteredPokemons;
 }
 
+
 export const orderData = (myPokemon, name, alfaOrder) => {
     const direction = (alfaOrder === "az") ? 1 : -1;
     return myPokemon.sort((a, b) => {
-        if (b[name] < a[name]) return direction;
-        else if (a[name] < b[name]) return -direction;
-        else return 0;
+        if (b.name < a.name) {
+            return direction;
+        } else {
+            return -direction;
+        }
     })
 }
 
 export const orderPokedex = (myPokemon, id, alfaOrder) => {
     if (alfaOrder == "pokedex") {
         return myPokemon.sort((a, b) => {
-            if (b[id] < a[id]) return 1;
-            else if (a[id] < b[id]) return -1;
-            else return 0;
+            if (b.id < a.id) {
+                return 1;
+            } else {
+                return -1;
+            }
         })
     }
 }
